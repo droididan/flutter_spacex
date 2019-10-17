b28aa009545c03f40488d1ff07c350ddb007790f
Idan Ayalon <idan@l.com>
2019-10-17 10:07:11 +0300

    generated file

---
+++ lib/feature/missions/data/model/mission_model.g.dart
@@ -0,0 +1,93 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'mission_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MissionResponseAdapter extends TypeAdapter<MissionResponse> {
+  @override
+  MissionResponse read(BinaryReader reader) {
+    var numOfFields = reader.readByte();
+    var fields = <int, dynamic>{
+      for (var i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return MissionResponse(
+      (fields[0] as List)?.cast<MissionModel>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, MissionResponse obj) {
+    writer
+      ..writeByte(1)
+      ..writeByte(0)
+      ..write(obj.launches);
+  }
+}
+
+class MissionModelAdapter extends TypeAdapter<MissionModel> {
+  @override
+  MissionModel read(BinaryReader reader) {
+    var numOfFields = reader.readByte();
+    var fields = <int, dynamic>{
+      for (var i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return MissionModel(
+      fields[0] as String,
+      fields[1] as String,
+      fields[2] as String,
+      fields[3] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, MissionModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.details)
+      ..writeByte(1)
+      ..write(obj.missionName)
+      ..writeByte(2)
+      ..write(obj.launchYear)
+      ..writeByte(3)
+      ..write(obj.id);
+  }
+}
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MissionResponse _$MissionResponseFromJson(Map<String, dynamic> json) {
+  return MissionResponse(
+    (json['launches'] as List)
+        ?.map((e) =>
+            e == null ? null : MissionModel.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
+}
+
+Map<String, dynamic> _$MissionResponseToJson(MissionResponse instance) =>
+    <String, dynamic>{
+      'launches': instance.launches,
+    };
+
+MissionModel _$MissionModelFromJson(Map<String, dynamic> json) {
+  return MissionModel(
+    json['details'] as String,
+    json['mission_name'] as String,
+    json['launch_year'] as String,
+    json['id'] as String,
+  );
+}
+
+Map<String, dynamic> _$MissionModelToJson(MissionModel instance) =>
+    <String, dynamic>{
+      'details': instance.details,
+      'mission_name': instance.missionName,
+      'launch_year': instance.launchYear,
+      'id': instance.id,
+    };
